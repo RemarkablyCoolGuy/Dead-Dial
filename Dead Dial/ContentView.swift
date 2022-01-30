bd70d091dead2e2bd5408ccf04d777af40d98327
Samuel Sullins <samuelasullins@gmail.com>
2022-01-29 23:33:38 -0700

    Initial Commit

---
+++ Dead Dial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dead Dial
+//
+//  Created by Samuel Sullins on 1/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dead Dial/Dead_DialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dead_DialApp.swift
+//  Dead Dial
+//
+//  Created by Samuel Sullins on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Dead_DialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
